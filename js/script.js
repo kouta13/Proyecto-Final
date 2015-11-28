a3f4a1a54a7ba63935993386103ccee4471d4a1e
Arnulfo Alejandro Beltran Soto <a282116@uach.mx>
2015-11-27 22:17:58 -0700

    6to Commit Ahora con un nuevo amigo
    Se agrega contacto.html, se agregan algunos estilos y scripts, se
    agregan imagenes y una modificacion con el index

--- js/script.js
+++ js/script.js
@@ -3,4 +3,8 @@ $(document).ready(function(){
     $( "#img-cron" ).slideToggle( "slow", function() {
     });
   });
+  $( "#icon-what" ).click(function() {
+    $( "#img-tel" ).slideToggle( "slow", function() {
+    });
+  });
 });
